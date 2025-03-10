c6a9968c4a80a137004ba8c029a2415fa5b04577
JoseVColino <josevictorcolino@gmail.com>
2025-03-10 12:30:32 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,13 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         DoublyLinkedList<String> list = new DoublyLinkedList<>();
         list.insertAtHead("0000");
-        list.insertAtHead("112");
+        list.insertAtHead("2");
         list.insertAtHead("1211");
         list.insertAtTail("-11");
         list.insertAtTail("-20000");
 
-        list.removeLongs(4);
-        list.readFromTail();
+        list.readFromHead();
+        list.insertAtIndex(2, "novo");
+        System.out.println();
+        list.readFromHead();
+        list.removeAtIndex(3);
+        System.out.println();
+
+        list.readFromHead();
+
         System.out.println();
     }
 }
